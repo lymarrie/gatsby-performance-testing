364e554dac2f7dd359bd58cf056073ad14dc5fcc
lymarrie <lucmarrie1@gmail.com>
2021-04-27 15:14:28 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: "gatsby-performance-testing",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "_cP38r9APQev4e4sUUfapmtQUEDGGN6ApKNPFVHcPJ8",
+        spaceId: "",
+      },
+    },
+  ],
+};
